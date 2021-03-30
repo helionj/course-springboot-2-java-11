51fed8192520c0932c4e402c0035997ac0286fb5
helionporto <helionporto@hotmail.com>
2021-03-30 14:02:22 -0400

    Service layer, component registration

--- src/main/java/com/helion/course/repositories/UserRepository.java
+++ src/main/java/com/helion/course/repositories/UserRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.helion.course.entities.User;
 
-public interface UserRespository extends JpaRepository<User, Long>{
+public interface UserRepository extends JpaRepository<User, Long>{
 
 }
--- src/main/java/com/helion/course/resources/UserResource.java
+++ src/main/java/com/helion/course/resources/UserResource.java
@@ -1,20 +1,35 @@
 package com.helion.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.helion.course.entities.User;
+import com.helion.course.services.UserService;
 
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User user = new User(1L, "Maria", "maria@email.com", "99991456", "123456");
+	public ResponseEntity<List<User>> findAll(){
+		List<User> users = service.findAll();
+		return ResponseEntity.ok().body(users);
+	}
+	
+	@GetMapping(value ="/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User user = service.findById(id);
 		return ResponseEntity.ok().body(user);
 	}
-}
+	
+}	
