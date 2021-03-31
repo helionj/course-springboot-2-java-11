79188a20e5055ebf792cd6e83ef4308285990728
helionporto <helionporto@hotmail.com>
2021-03-30 20:47:48 -0400

    Cetgory

--- src/main/java/com/helion/course/resources/CategoryResource.java
+++ src/main/java/com/helion/course/resources/CategoryResource.java
@@ -9,27 +9,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.helion.course.entities.Order;
-import com.helion.course.services.OrderService;
+import com.helion.course.entities.Category;
+import com.helion.course.services.CategoryService;
 
 
 @RestController
-@RequestMapping(value = "/orders")
-public class OrderResource {
+@RequestMapping(value = "/categories")
+public class CategoryResource {
 
 	@Autowired
-	private OrderService service;
+	private CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Order>> findAll(){
-		List<Order> orders = service.findAll();
-		return ResponseEntity.ok().body(orders);
+	public ResponseEntity<List<Category>> findAll(){
+		List<Category> categorys = service.findAll();
+		return ResponseEntity.ok().body(categorys);
 	}
 	
 	@GetMapping(value ="/{id}")
-	public ResponseEntity<Order> findById(@PathVariable Long id){
-		Order order = service.findById(id);
-		return ResponseEntity.ok().body(order);
+	public ResponseEntity<Category> findById(@PathVariable Long id){
+		Category category = service.findById(id);
+		return ResponseEntity.ok().body(category);
 	}
 	
 }	
