c50ad8fc04ae6b994a059b58ac3981866328dfd8
helionporto <helionporto@hotmail.com>
2021-04-01 11:17:14 -0400

    Exception handler findbyId

--- src/main/java/com/helion/course/services/UserService.java
+++ src/main/java/com/helion/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.helion.course.entities.User;
 import com.helion.course.repositories.UserRepository;
+import com.helion.course.services.exception.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = userRepository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
