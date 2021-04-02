a04dfb786b2abbf2f55fc584442265d57dd1cf51
helionporto <helionporto@hotmail.com>
2021-04-01 20:06:07 -0400

    Insert, delete, update ProductService and ProductResource

--- src/main/java/com/helion/course/resources/ProductResource.java
+++ src/main/java/com/helion/course/resources/ProductResource.java
@@ -1,15 +1,22 @@
 package com.helion.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.helion.course.entities.Product;
+import com.helion.course.entities.User;
 import com.helion.course.services.ProductService;
 
 
@@ -30,6 +37,26 @@ public class ProductResource {
 	public ResponseEntity<Product> findById(@PathVariable Long id){
 		Product product = service.findById(id);
 		return ResponseEntity.ok().body(product);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Product> insert(@RequestBody Product obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value ="/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value ="/{id}")
+	public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product obj){
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 	
 }	
--- src/main/java/com/helion/course/services/ProductService.java
+++ src/main/java/com/helion/course/services/ProductService.java
@@ -3,26 +3,65 @@ package com.helion.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.helion.course.entities.Product;
+import com.helion.course.entities.Product;
 import com.helion.course.repositories.ProductRepository;
+import com.helion.course.services.exception.DatabaseException;
+import com.helion.course.services.exception.ResourceNotFoundException;
 
 @Service
 public class ProductService {
 
 	@Autowired
 	private ProductRepository productRepository;
-	
-	public List<Product> findAll(){
+
+	public List<Product> findAll() {
 		return productRepository.findAll();
 	}
-	
+
 	public Product findById(Long id) {
 		Optional<Product> obj = productRepository.findById(id);
 		return obj.get();
 	}
-	
-	
+
+	public Product insert(Product obj) {
+		return productRepository.save(obj);
+	}
+
+	public void delete(Long id) {
+		try {
+			productRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+
+	}
+
+	public Product update(Long id, Product obj) {
+		try {
+			Product entity = productRepository.getOne(id);
+			updateEntity(entity, obj);
+			return productRepository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+
+	}
+
+	private void updateEntity(Product entity, Product obj) {
+		entity.setName(obj.getName());
+		entity.setDescription(obj.getDescription());
+		entity.setPrice(obj.getPrice());
+		entity.setImgUrl(obj.getImgUrl());
+	}
+
 }
